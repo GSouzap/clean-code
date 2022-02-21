4baa05c7e2189799c0721485e25387e57ad8a92a
Biel <gabriel.pinto@bitcapital.com.br>
2022-02-21 20:34:30 -0300

    feat: add ExpressRouteAdapter

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://biel:kmg25417@localhost:27017/admin',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050
 }
+// mongodb://biel:kmg25417@localhost:27017/clean-node-api?authSource=admin
